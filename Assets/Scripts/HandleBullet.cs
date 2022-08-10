939936b6343a830325320dd5d3500606b399ccc5
SachinAngira <sachin.angira@skidos.com>
2022-08-10 13:39:10 +0530

    Inital Commit

---
+++ Assets/Scripts/HandleBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleBullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Contains("Bullet"))
+        {
+        
+            Destroy(collision.gameObject); 
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject GameoverUI;
+    [SerializeField]
+    private Text GameoverText;
+
+    public static UIController Instance;
+
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public void SetGameOverText(string s)
+    {
+        GameoverText.text = s;
+        Time.timeScale = 0f;
+        GameoverUI.SetActive(true);
+    }
+    public void Replay()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+}
